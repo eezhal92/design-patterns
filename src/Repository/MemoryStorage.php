4de95ca2bd30004bdf330146829ddc41a24e31f4
eezhal92 <arkinthesky.69@gmail.com>
2016-03-28 22:11:46 +0700

    repository pattern

---
+++ src/Repository/MemoryStorage.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Eezhal92\Repository;
+
+class MemoryStorage implements StorageInterface
+{
+    private $lastId = 0;
+
+    private $data = [];
+
+    public function persist($data)
+    {
+        $this->data[++$this->lastId] = $data;
+
+        return $this->lastId;
+    }
+
+    public function retrieve($id)
+    {
+        return $this->exists($id) ? $this->data[$id] : null;
+    }
+
+    public function delete($id){
+        if (! $this->exists($id)) {
+            return false;
+        }
+
+        $this->data[$id] = null;
+        unset($this->data[$id]);
+
+        return true;
+    }
+
+    private function exists($id)
+    {
+        return isset($this->data[$id]);
+    }
+}
