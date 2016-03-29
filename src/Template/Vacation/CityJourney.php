e08cf9adbc2c3a5f87532a16c308363ce1b9868d
eezhal92 <arkinthesky.69@gmail.com>
2016-03-29 20:17:37 +0700

    template pattern

---
+++ src/Template/Vacation/CityJourney.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Eezhal92\Template\Vacation;
+
+class CityJourney extends Journey
+{
+    public function enjoy()
+    {
+        echo "Walking and get lost\n";
+    }
+}
---
+++ src/Template/Vacation/Journey.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Eezhal92\Template\Vacation;
+
+abstract class Journey
+{
+    final public function takeTrip()
+    {
+        $this->buyAFlight();
+        $this->takePlane();
+        $this->enjoy();
+        $this->buyGift();
+        $this->takePlane();
+    }
+
+    protected abstract function enjoy();
+
+    protected function buyGift()
+    {
+
+    }
+
+    private function buyAFlight()
+    {
+        echo "Buy a flight\n";
+    }
+
+    private function takePlane()
+    {
+        echo "Taking the plane\n";
+    }
+}
